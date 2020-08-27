fa1c52f698f98e4997ee2c62ba431fc805b7aad2
Eivind Støland <eivinsto@student.matnat.uio.no>
2020-08-27 12:02:03 +0200

    Fixed merge error shenanigans

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,15 @@ using namespace arma;
 
 int main(int argc, char** argv)
 {
-<<<<<<< HEAD
+
   int N = atoi(argv[1]);
-  vec x = linspace<vec>(0, 1, N);
 
-  cout << x << N << endl;
-=======
-  int n = atoi(argv[1]);
+  vec x = linspace<mat>(0,1,N);
 
-  vec x = linspace<mat>(0,1,n);
-
-  vec f = zeros<mat>(n);
-  for (int i = 0; i<n; ++i){
+  vec f = zeros<mat>(N);
+  for (int i = 0; i<N; ++i){
     f[i] = 100*exp(-10*x[i]);
   }
->>>>>>> 834ee3c222ea880e11bcc78673f509b7c29951b0
 
   return 0;
 }
