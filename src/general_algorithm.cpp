dbffc51cbe69f093474e13a55e531c768e7ada3b
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-09-02 15:14:28 +0200

    Reduced number of arrays, and made array names match problem text

--- src/general_algorithm.cpp
+++ src/general_algorithm.cpp
@@ -9,14 +9,10 @@ using namespace arma;
 void general_forward(vec& a, vec& b, vec& c, vec& b_twiddle, int N)
 {
   //setting first elements
-  double b_temp = b[1];
-  double b_twiddle_temp = b_twiddle[1];
   //forward loop
   for (int i = 1; i<N; ++i){
-    b[i] = b_temp - a[i-1]*c[i-1]/b[i-1];
-    b_twiddle[i] = b_twiddle_temp - a[i-1]*b_twiddle[i-1]/b[i-1];
-    b_temp = b[i+1];
-    b_twiddle_temp = b_twiddle[i+1];
+    b[i] = b[i]- a[i-1]*c[i-1]/b[i-1];
+    b_twiddle[i] = b_twiddle[i]- a[i-1]*b_twiddle[i-1]/b[i-1];
   }
 }
 
