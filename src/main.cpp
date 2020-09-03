2ba8d7ee03ede1e09e2438202ce744abf386fa8e
Anders Petershagen Åsbø <anderpaa@uio.no>
2020-09-03 10:41:32 +0200

    Cleaned up main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,36 +9,15 @@ using namespace arma;
 
 int main(int argc, char** argv)
 {
-
   int N = atoi(argv[1]);            // Step count
-  vec x = linspace<vec>(0,1,N);     // Vector for x-values.
-  vec a(N-1); a.fill(-1);           // Vector for lower diagonal.
-  vec b(N); b.fill(2);              // Vector for diagonal.
-  vec c(N-1); c.fill(-1);           // Vector for upper diagonal.
-  vec u(N);                         // Vector for numerical solution
   double h = 1.0/(N+1);             // Step length
   //mat A = zeros<mat>(N,N);          // Matrix to use with LU decomposition
-
-  // Generating values for input function
-  vec b_twiddle = zeros<vec>(N);
-  for (int i = 0; i<N; ++i){
-    b_twiddle[i] = h*h*100*exp(-10*x[i]);
-  }
-
-  // Generating values for analytic solution
-  vec u_anal = zeros<vec>(N);
-  for (int i=0; i<N; ++i){
-    u_anal[i] = 1 - (1 - exp(-10))*x[i] - exp(-10*x[i]);
-  }
-
-  // Declaring variables for CPU time measurement
-  clock_t start, finish;
 
   // General algorithm call
   general_main(N,h);
 
-
-  special_main(N, h); // performing special_algorithm with benchmark.
+  // performing special_algorithm with benchmark.
+  special_main(N, h);
 
 
   // Generate matrix values to use with LU decomposition
