64d9c44e4e29de9417a048658fd554a4c299c221
Anders Petershagen Åsbø <aanders35@gmail.com>
2020-09-05 16:24:06 +0200

    comments

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -5,19 +5,26 @@ import matplotlib as mpl
 
 
 def main():
-    mpl.rcParams.update({"text.usetex": True})
+    """
+    This program solves Poisson's eq with thomas algorithm
+    and lu decomposition in cpp, and plots the result against
+    an analytic solution.
+    """
+    mpl.rcParams.update({"text.usetex": True})  # using latex.
 
-    N = np.array([10, 100, 1000])
-    call(["make"])
+    N = np.array([10, 100, 1000])  # array of N values.
+    call(["make"])  # compiling any changed cpp files.
     for i in N:
-        run(["./main.exe", f"{i}"])
+        run(["./main.exe", f"{i}"])  # running algorithms for each N.
 
+        # reading data from file:
         x = np.fromfile(f"x{i}.bin")
         u_anal = np.fromfile(f"u_anal{i}.bin")
         u_special = np.fromfile(f"u_special{i}.bin")
         u_general = np.fromfile(f"u_general{i}.bin")
         u_LUdecomp = np.fromfile(f"u_LUdecomp{i}.bin")
 
+        # plotting each numerical solve vs analytic solve:
         plt.figure()
         plt.plot(x, u_anal, label="analytic solution")
         plt.plot(x, u_general, label="numerical solution")
@@ -45,8 +52,8 @@ def main():
         plt.legend()
         plt.grid()
 
-    call(["make", "cleanbin"])
-    plt.show()
+    call(["make", "cleanbin"])  # deleting data files.
+    plt.show()  # displaying plots
 
 
 if __name__ == '__main__':
